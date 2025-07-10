af1f1e7b48db1d219181be6fc7f9ab55b465d124
David <dromang5@gmail.com>
2025-07-10 15:05:41 +0200

    Fix error code in file

--- Poms/loginPage.ts
+++ Poms/loginPage.ts
@@ -1,13 +1,18 @@
 import { Page, Locator } from "playwright/test";
 
-export default class LoginPage {
+export class LoginPage {
+	readonly page: Page;
 	private readonly usernameTextbox: Locator;
 	private readonly passTextbox: Locator;
 	private readonly loginButton: Locator;
 
 	constructor(page: Page) {
+		this.page = page;
 		this.usernameTextbox = page.getByRole("textbox", { name: "Username" });
 		this.passTextbox = page.getByRole("textbox", { name: "Password" });
 		this.loginButton = page.getByRole("button", { name: "Login" });
+	}
+	async fillUsername() {
+		this.usernameTextbox.fill("Standard_user");
 	}
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import LoginPage from "../Poms/LandingPage";
+import { LoginPage } from "../Poms/loginPage";
 
 test("Evaluate login fields", async ({ page }) => {
 	await page.goto("https://www.saucedemo.com/");
@@ -12,5 +12,5 @@ test("Login in to page", async ({ page }) => {
 
 	await page.goto("https://www.saucedemo.com/");
 	await page.waitForTimeout(5000);
-	await page.pause();
+	await loginPage.fillUsername();
 });
