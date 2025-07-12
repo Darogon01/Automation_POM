9e1494bb623f2dec24f70e76c1de543fe03af417
DaviDev <dromang5@gmail.com>
2025-07-12 18:26:20 +0200

    Fix Pom to execute in test

--- Poms/loginPage.ts
+++ Poms/loginPage.ts
@@ -1,18 +1,27 @@
 import { Page, Locator } from "playwright/test";
 
 export class LoginPage {
-	readonly page: Page;
-	 readonly usernameTextbox: Locator;
-	 readonly passTextbox: Locator;
-	private readonly loginButton: Locator;
+    readonly page: Page;
+    readonly usernameTextbox: Locator;
+    readonly passTextbox: Locator;
+    readonly loginButton: Locator;
 
-	constructor(page: Page) {
-		this.page = page;
-		this.usernameTextbox = page.getByRole("textbox", { name: "Username" });
-		this.passTextbox = page.getByRole("textbox", { name: "Password" });
-		this.loginButton = page.getByRole("button", { name: "Login" });
-	}
-	async fillUsername() {
-		this.usernameTextbox.fill("Standard_user");
-	}
+    constructor(page: Page) {
+        this.page = page;
+        this.usernameTextbox = page.getByRole("textbox", { name: "Username" });
+        this.passTextbox = page.getByRole("textbox", { name: "Password" });
+        this.loginButton = page.getByRole("button", { name: "Login" });
+    }
+    async fillUsername() {
+        // this.usernameTextbox.waitFor({ state: "visible" });
+        await this.usernameTextbox.fill("standard_user");
+    }
+
+    async fillPass() {
+        await this.passTextbox.fill("secret_Sauce");
+    }
+
+    async clickLoginButton() {
+        await this.loginButton.click();
+    }
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,15 +2,15 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../Poms/loginPage";
 
 test("Evaluate login fields", async ({ page }) => {
-	await page.goto("https://www.saucedemo.com/");
-	await page.waitForTimeout(5000);
-	await page.pause();
+    await page.goto("https://www.saucedemo.com/");
+    await page.waitForTimeout(5000);
 });
 
 test("Login in to page", async ({ page }) => {
-	const loginPage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
 
-	await page.goto("https://www.saucedemo.com/");
-	await expect(loginPage.usernameTextbox).toBeVisible();
-	await loginPage.fillUsername();
+    await page.goto("https://www.saucedemo.com/");
+    await loginPage.fillUsername();
+    await loginPage.fillPass();
+    await loginPage.clickLoginButton();
 });
